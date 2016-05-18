208d5bd4e48d63689062b5ff1cf053fecbcb10fa
Christopher Bradhaw <christopher.bradshaw@clever.com>
2016-05-18 03:43:18 +0000

    first thing that might work

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
-	"github.com/Christopher-Bradshaw/caustic/oauth"
+	"github.com/sshleifer/caustic/oauth"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	oauth.RedirectToCoursera()
-	oauth.GetToken("blah")
+	r := mux.NewRouter()
+	// Redirect to coursera
+	r.Methods("GET").Path("/coursera-login").HandlerFunc(oauth.RedirectToCoursera)
+	// URL that coursera redirects to
+	r.Methods("GET").Path("/back-at-you").HandlerFunc(oauth.RedeemCode)
 }
--- oauth/oauth.go
+++ oauth/oauth.go
@@ -4,18 +4,19 @@ package oauth
 
 import (
 	"fmt"
+	"net/http"
 	"net/url"
 )
 
 const (
 	clientID     = "cBiYd-vLLx-7_BV8AZ1jGQ"
 	clientSecret = "A5HoPutMFYTXcySPk4c6sw"
-	redirectURI  = "http://christopherbradshaw.me:5001"
+	redirectURI  = "http://christopherbradshaw.me/back-at-you:5001"
 	csrfState    = "yoyoyo"
 )
 
 // This is the url that we need to redirect incoming users to
-func RedirectToCoursera() string {
+func RedirectToCoursera(w http.ResponseWriter, r *http.Request) {
 	baseUrl := "https://accounts.coursera.org/oauth2/v1/auth"
 
 	v := url.Values{}
@@ -25,11 +26,31 @@ func RedirectToCoursera() string {
 	v.Set("scope", "view_profile")
 	v.Set("state", csrfState)
 
-	fmt.Println(baseUrl + "?" + v.Encode())
-	return baseUrl + "?" + v.Encode()
+	fullURL := baseUrl + "?" + v.Encode()
+	fmt.Println(fullURL)
+	http.Redirect(w, r, fullURL, 301)
 }
 
-func parseResponse(inUrl string) {
+// Once users are back with us we can take the code they gave us to go to coursera and get a token
+func RedeemCode(w http.ResponseWriter, r *http.Request) {
+	baseUrl := "https://accounts.coursera.org/oauth2/v1/token"
+
+	v := url.Values{}
+	v.Set("code", getCodeFromUrl(r.URL.String()))
+	v.Set("client_id", clientID)
+	v.Set("client_secret", clientSecret)
+	v.Set("redirect_uri", redirectURI)
+	v.Set("grant_type", "authorization_code")
+
+	fullURL := (baseUrl + "?" + v.Encode())
+	resp, err := http.Post(fullURL, "blah", nil)
+	if err != nil {
+		panic(fmt.Sprintf("err in parsing code response: %s", err.Error()))
+	}
+	fmt.Printf("%#v\n", resp.Body)
+}
+
+func getCodeFromUrl(inUrl string) string {
 	parsedUrl, err := url.Parse(inUrl)
 
 	if err != nil {
@@ -44,20 +65,9 @@ func parseResponse(inUrl string) {
 	if v["state"][0] != csrfState {
 		panic(fmt.Sprintf("csrf state yo!: %s", v["state"]))
 	}
-}
 
-// Once users are back with us we can take the code they gave us to go to coursera and get a token
-func GetToken(code string) string {
-	baseUrl := "https://accounts.coursera.org/oauth2/v1/token"
-
-	v := url.Values{}
-	v.Set("code", code)
-	v.Set("client_id", clientID)
-	v.Set("client_secret", clientSecret)
-	v.Set("redirect_uri", redirectURI)
-	v.Set("grant_type", "authorization_code")
-
-	fmt.Println(baseUrl + "?" + v.Encode())
-	return baseUrl + "?" + v.Encode()
-	// Post to this url to get the token
+	if len(v["code"][0]) != 1 {
+		panic(fmt.Sprintf("no code yo!: %s", v["code"]))
+	}
+	return v["code"][0]
 }
